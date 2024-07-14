256e841dbf5fa0e8e12a227ed061c90c30cad6ee
mxxxxxxy <13979126942@qq.com>
2024-07-14 21:11:43 +0800

    add timeline

---
+++ Meng/src/components/timeline.vue
@@ -0,0 +1,30 @@
+<template>
+    <svg width="100%" height="100%">
+
+    </svg>
+</template>
+
+
+<script>
+
+
+export default{
+    data() {
+        return {
+            
+        }
+    },
+    methods:{
+
+    },
+    mounted(){
+        console.log('success')
+    },
+
+}
+
+</script>
+
+<style>
+
+</style>
