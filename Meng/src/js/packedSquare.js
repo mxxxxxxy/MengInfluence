a8a18fdb28543d2100ca041ad7d4f4af26799aa5
mxxxxxxy <13979126942@qq.com>
2024-07-15 19:38:45 +0800

    hier convert

--- Meng/src/js/packedSquare.js
+++ Meng/src/js/packedSquare.js
@@ -1,14 +1,16 @@
+
 export default function() {
   var dx = 1,
       dy = 1,
+      depth = 1,
       padding = 0,
       round = false;
 
   function partition(root) {
     // var n = root.height + 1;
     root.x0 = root.y0 = padding;
-    root.x1 = dx;
-    root.y1 = dy;
+    root.x1 = dx - padding;
+    root.y1 = dy - padding;
     root.eachBefore(positionNode(dy));
     if (round) root.eachBefore(roundNode);
     return root;
@@ -16,15 +18,16 @@ export default function() {
 
   function positionNode(dy) {
     return function(node) {
-      if (node.children) {
-        Dice(node, node.x0 + padding, node.y0 + padding, node.x1, dy);
+      if (node.children && depth > 0) {
+        depth -= 1
+        Dice(node, node.x0 + padding, node.y0 + padding, node.x1 - padding, node.y1 - padding, padding);
       }
       var x0 = node.x0 ,
           y0 = node.y0 ,
-          x1 = node.x1 - (padding * node.depth),
-          y1 = node.y1 - (padding * node.depth);
-      if (x1 < x0) x0 = x1 = (x0 + x1) / 2
-      if (y1 < y0) y0 = y1 = (y0 + y1) / 2;
+          x1 = node.x1,
+          y1 = node.y1;
+      if (x1 < x0) x0 = x1 = (x0 + x1); //padding大于width的情况下让width = 0
+      if (y1 < y0) y0 = y1 = (y0 + y1); //padding大于height的情况下让height = 0
       node.x0 = x0;
       node.y0 = y0;
       node.x1 = x1;
@@ -36,6 +39,10 @@ export default function() {
     return arguments.length ? (round = !!x, partition) : round;
   };
 
+  partition.set_depth = function(x) {
+    return arguments.length ? (depth = x, partition) : depth;
+  };
+
   partition.size = function(x) {
     return arguments.length ? (dx = +x[0], dy = +x[1], partition) : [dx, dy];
   };
@@ -44,21 +51,29 @@ export default function() {
     return arguments.length ? (padding = +x, partition) : padding;
   };
 
+  partition.find_node_and_get_first_descendants = function(_node){
+    const found = this.find(node => concatName(node) === concatName(_node));
+    return [found, ...found.children]
+}
+
   return partition;
 }
 
-function Dice(parent, x0, y0, x1, y1) {
+
+
+function Dice(parent, x0, y0, x1, y1, padding) {
     var nodes = parent.children,
     node,
     i = -1,
     n = nodes.length,
-    k = parent.value && (x1 - x0) / parent.value;
+    k = parent.value && (x1 - x0 - (n-1) * padding) / parent.value;
     while (++i < n) {
         node = nodes[i];
         node.y0 = y0;
         node.y1 = y1;
-        node.x0 = x0, 
-        node.x1 = x0 += (node.value * k);
+        node.x0 = x0;
+        node.x1 = x0 + node.value * k;
+        x0 = node.x1 + padding;
     }
 }
 
--- Meng/src/js/tree.js
+++ Meng/src/js/tree.js
@@ -1,31 +1,57 @@
 import * as d3 from 'd3';
+import { concatName } from '@/js/utils'
 
 export default function(book){
-    var hier = d3.hierarchy(book).sum(d=> d.is_leaf ? d.value.length : 0);
-    hier.each( n => n.cite = [[],[],[]])
-    // 统计引用梦的条目
-    hier.eachAfter( (current) => {
-        if(current.data.is_leaf == true){
-            current.cite[2] = current.data.value.map(d=>d['梦溪笔谈条目序数']);
-        }
-        if(current.parent){
-            current.parent.cite[2] = current.parent.cite[2].concat(current.cite[2]);
-        }
-    })
-    // 统计引用梦的卷题
-    hier.eachAfter( (current, idx) => { 
-        if(current.data.is_leaf == true){
-            current.cite[1] = current.data.value.map(d=>d['卷题']);
-        }
-        if(current.parent){
-            current.parent.cite[1] = current.parent.cite[1].concat(current.cite[1]);
-        }
-    })
+    var hier = d3.hierarchy(book).sum(d=> d.is_leaf ? 1 : 0);
+
+
+    // 给引用梦溪笔谈的书加上引用的数据
+    if(book.name !== "meng"){
+        hier.each( n => n.cite = [[],[],[],[]])
+        // 统计引用梦的条目
+        hier.eachAfter( (current) => {
+            if(current.data.is_leaf == true){
+                current.cite[3] = current.data.value.map(d=>d['梦溪笔谈条目序数']);
+            }
+            if(current.parent){
+                current.parent.cite[3] = current.parent.cite[3].concat(current.cite[3]);
+            }
+        })
+        // 统计引用梦的卷题
+        hier.eachAfter( (current, idx) => { 
+            if(current.data.is_leaf == true){
+                current.cite[2] = current.data.value.map(d=>d['卷题']);
+            }
+            if(current.parent){
+                current.parent.cite[2] = current.parent.cite[2].concat(current.cite[2]);
+            }
+        })
+
+        // 统计引用梦的卷题类
+        hier.eachAfter( (current, idx) => { 
+            if(current.data.is_leaf == true){
+                current.cite[1] = current.data.value.map(d=>d['卷题类']);
+            }
+            if(current.parent){
+                current.parent.cite[1] = current.parent.cite[1].concat(current.cite[1]);
+            }
+        })
+    }
+
+    //获取某一层的节点
     hier.get_nodes_by_depth = function(depth){
         return this.descendants().filter(_ => _.depth == depth);
     }
+
+    // 获取某一层cite了梦m_depth的query的节点
     hier.get_cited_nodes_by_depth = function(depth, m_depth, query){
         return this.descendants().filter(_ => _.depth == depth && _.cite[m_depth].includes(query));
     }
+
+    hier.find_node_and_get_first_descendants = function(_node){
+        const found = this.find(node => concatName(node) === concatName(_node));
+        return [found, ...found.children]
+    }
+    
     return hier
 }
